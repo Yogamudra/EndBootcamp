6dfd8cebdbed982c4ee28201b206a965fb7d9c7b
«Elena <svetelenn@gmail.com>
2022-10-03 03:33:56 +0800

    source

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"Geek", "Brains", "ok", "edu", "yes"};
+string[] array2 = new string[5];
+void ArrayString(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[k] = array1[i];
+        k++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayString(array1, array2);
+PrintArray(array2);
